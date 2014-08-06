245df05c37d09521cf50b73eb24df62bb42d6aa8
Mohamed Abidi <mabidi6@gmail.com>
2014-08-06 10:29:06 +0100

    Fixed bower.json

---
+++ bower.js
@@ -0,0 +1,11 @@
+{
+    "name": "jquery-form-dependencies",
+    "versin": "0.0.2",
+    "main": "./jquery.form-dependencies.js",
+    "ignore": [
+        'README',
+        'test/index.html',
+        'test/jquery-1.6.1.js'
+    ]
+
+}
